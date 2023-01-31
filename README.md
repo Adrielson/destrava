005d1fe38c17e06360585edfcc370dfd36eb6f85
Adrielson <adrielferreira28@gmail.com>
2023-01-30 23:34:59 -0300

    Adicionei o a conexão com o banco de dados
    Adicionei uma pasta SQL para os arquivos de criação e gatilhos. Foi adicionado a conexão com o banco no arquivo entrar.

--- README.md
+++ README.md
@@ -4,4 +4,11 @@ O destrava visa facilitar o encontro entre professores (empregados ou não) que 
 ### Organização diretórios
 
 - No diretório [img](https://github.com/Adrielson/destrava/tree/main/img) estão as imagens utilizadas no projeto.
-- No diretório [docs](https://gitlab.com/Rivail/classify/-/tree/main/design) estão os arquivos e link para os diversos arquivos adicionais ao projeto.
+- No diretório [docs](https://github.com/Adrielson/destrava/tree/main/docs) estão os arquivos e link para os diversos arquivos adicionais ao projeto.
+- No diretório [sql](https://github.com/Adrielson/destrava/tree/main/sql) estão os arquivos de criação do banco de dados. 
+  Além da criação das tabelas há um script para criação de gatilhos, sendo eles:
+    - gatilho pra adicionar id a tabela alunos
+    - gatilho pra adicionar id a tabela professores
+    - gatilho para validar email da tabela a professores
+    - gatilho para validar email da tabela a alunos
+
--- entrar.php
+++ entrar.php
@@ -1,3 +1,53 @@
+<?php
+
+
+if (isset($_POST['cadastrar'])) {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $tipo = $_POST['tipo'];
+
+    // Criando conexão com o banco de dados
+    $conn = mysqli_connect('localhost', 'root', '', 'destrava');
+
+    // Verificando se houve erro de conexão
+    if (!$conn) {
+        die("Conexão falhou: " . mysqli_connect_error());
+    }
+    if (empty($tipo)) {
+        echo "Você precisa escolher uma opção!";
+    } else if ($tipo == "aluno") {
+        $query = "INSERT INTO alunos (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+        $result = mysqli_query($conn, $query);
+
+        if ($result) {
+            echo "<script> alert('Aluno cadastrado com sucesso!');
+            window.location.href='entrar.php';
+            </script>";
+        } else {
+            echo "<script> alert('Não foi possível realizar o cadastro!');
+            window.location.href='entrar.php';
+            </script>";
+        }
+    } else if ($tipo == "professor") {
+        $query = "INSERT INTO professores (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+        $result = mysqli_query($conn, $query);
+
+        if ($result) {
+            echo "<script> alert('Professor cadastrado com sucesso!');
+            window.location.href='entrar.php';
+            </script>";
+        } else {
+            echo "<script> alert('Não foi possível realizar o cadastro!');
+            window.location.href='entrar.php';
+            </script>";
+        }
+    }
+    mysqli_close($conn);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -30,65 +80,34 @@
                         Entrar <i class="fa-solid fa-arrow-right-to-bracket"></i>
                     </button>
                     <a class="nav-link active" aria-current="page" href="index.php">voltar para página inicial</a>
-                    <!-- <input type="submit" value="Entrar" class="btn solid" /> -->
-                    <!-- <p class="social-text">Or Sign in with social platforms</p>
-            <div class="social-media">
-              <a href="#" class="social-icon">
-                <i class="fab fa-facebook-f"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-twitter"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-google"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-linkedin-in"></i>
-              </a>
-            </div> -->
+
                 </form>
-                <form action="#" class="cadastrar-form">
+                <form action="" method="post" class="cadastrar-form"> <!-- action e o method -->
                     <h2 class="title">Junte-se a nós</h2>
                     <p class="descricao">Faça seu cadastro</p>
                     <div class="input-field">
                         <i class="fas fa-user"></i>
-                        <input type="nome" placeholder="Nome" />
+                        <input type="nome" name="nome" placeholder="Nome" /> <!-- atribui nome ao botao pra usar php -->
                     </div>
                     <div class="input-field">
                         <i class="fas fa-envelope"></i>
-                        <input type="email" placeholder="Email" />
+                        <input type="email" name="email" placeholder="Email" />
                     </div>
                     <div class="input-field">
                         <i class="fas fa-lock"></i>
-                        <input type="senha" placeholder="Senha" />
+                        <input type="senha" name="senha" placeholder="Senha" />
                     </div>
                     <div class="inline-form" for="inline-form">Você é aluno ou professor?
-                        <select class="custom-select">
-                            <option class="inline-option-escolher" value="aluno">escolher</option> <!-- adicionar erro -->
+                        <select class="custom-select" name="tipo">
+                            <option class="inline-option-escolher">escolher</option> <!-- adicionar erro -->
                             <option class="inline-option" value="aluno">aluno</option>
                             <option class="inline-option" value="professor">professor</option>
                         </select>
                     </div>
-                    <button class="btn">
-                        Cadastre-se <i class="fa-solid fa-user-plus"></i>
-                    </button>
+
                     <a class="nav-link active" aria-current="page" href="index.php">voltar para página inicial</a>
-                    <!-- <input type="submit" class="btn" value="Cadastrar" /> -->
-                    <!-- <p class="social-text">Or Sign up with social platforms</p>
-            <div class="social-media">
-              <a href="#" class="social-icon">
-                <i class="fab fa-facebook-f"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-twitter"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-google"></i>
-              </a>
-              <a href="#" class="social-icon">
-                <i class="fab fa-linkedin-in"></i>
-              </a>
-            </div> -->
+                    <input type="submit" name="cadastrar" class="btn" value="Cadastrar">
+
                 </form>
             </div>
         </div>
